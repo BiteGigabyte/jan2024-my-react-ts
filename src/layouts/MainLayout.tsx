2d93f1c728a5e6d9377393ccd9ff7cce3d76498d
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2024-06-01 21:16:12 +0300

    Виконав завдання уроку 9 (переписав з лекції ніби розуміння є)

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Outlet} from "react-router-dom";
+
+import {HeaderComponent} from "../component/HeaderComponent";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <HeaderComponent/>
+            <Outlet/>
+        </div>
+    );
+};
+
+export {MainLayout};
---
+++ src/pages/AuthPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {FormComponent} from "../component/FormComponent";
+
+const AuthPage = () => {
+    return (
+        <div>
+            <FormComponent/>
+            
+        </div>
+    );
+};
+
+export {AuthPage};
