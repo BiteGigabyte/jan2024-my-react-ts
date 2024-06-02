595c98e08a890edb1a3a42a293e22ab22dc5fdeb
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2024-06-02 16:16:24 +0300

    Виконую завдання уроку 10. Зробив репрезентаці сам урок про пагінацію почався з 14 хвилини.

--- src/pages/CarsPage.tsx
+++ src/pages/CarsPage.tsx
@@ -1,17 +1,29 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {carService} from "../services/api.service";
+import {CarsComponent} from "../component/CarsComponent";
+import {ICarPaginatedModel} from "../models/ICarPaginatedModel";
 
 const CarsPage = () => {
 
+    const [carsPaginatedObject, setCarsPaginatedObject] = useState<ICarPaginatedModel>({
+        items: [],
+        next: null,
+        prev: null,
+        total_items: 0,
+        total_pages: 0
+    })
+
     useEffect(() => {
-        carService.getCars().then(value => console.log(value));
+        carService.getCars().then(value => {
+            if (value) {
+                setCarsPaginatedObject(value);
+            }
+        });
     }, []);
 
     return (
         <div>
-
-            CarsPage
-
+            <CarsComponent cars={carsPaginatedObject.items}/>
         </div>
     );
 };
