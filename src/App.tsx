585825687c9d3c222e5c61b22c8c736d7e847365
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2024-06-15 13:40:29 +0300

    переписав 13 урок
    через redux
    
    Додати в існуючу логіку сторінку з комментарями та сторінку постів+ комментарів. Звісно все робиться через контекст та сховище

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.css';
 
-import React from 'react';
+import {decrement, increment, useAppDispatch, useAppSelector} from "./redux/store";
 
 const App = () => {
+
+    const {value} = useAppSelector(state => state.slice1);
+
+    const dispatch = useAppDispatch();
+
   return (
       <div>
-        App
-
+          <h2>{value}</h2>
+          <button onClick={() => {
+              dispatch(increment(2));
+          }}>increment</button>
+          <button onClick={() => {
+              dispatch(decrement());
+          }}>decrement</button>
       </div>
   );
 };
-
-export {App};
 
 export default App;
---
+++ src/slices/slice1.ts
@@ -0,0 +1,21 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+type CounterStateType = {
+    value: number;
+}
+
+const initialState: CounterStateType = {
+    value: 100500,
+};
+export const counter1Slice = createSlice({
+    name: "counter1",
+    initialState: initialState,
+    reducers: {
+        increment: (state, action: PayloadAction<number>) => {
+            state.value = state.value + action.payload;
+        },
+        decrement: (state) => {
+            state.value = state.value - 1;
+        }
+    }
+});
